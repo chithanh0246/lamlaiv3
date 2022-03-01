0873632b12a553d6fd421a5a614e28bd097d030b
HoaiPhong <vhphong2000@gmail.com>
2022-02-28 20:16:51 -0800

    phong commit

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -55,7 +55,7 @@ namespace WindowsFormsApp1
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(310, 184);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.Size = new System.Drawing.Size(67, 16);
             this.label2.TabIndex = 9;
             this.label2.Text = "Password";
             // 
@@ -74,9 +74,9 @@ namespace WindowsFormsApp1
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(310, 144);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.Size = new System.Drawing.Size(80, 16);
             this.label1.TabIndex = 10;
-            this.label1.Text = "UserName";
+            this.label1.Text = "UserName1";
             // 
             // Form1
             // 
